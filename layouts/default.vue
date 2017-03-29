24d772c3ef7ff593fc927f4eee87405b0b89701c
Michael Ilg <michael@ilg.ac>
2017-03-29 13:58:22 +0200

    Add layouts

---
+++ layouts/default.vue
@@ -0,0 +1,100 @@
+
+<template>
+  <v-app top-toolbar footer class="app">
+    <v-toolbar class="grey darken-3">
+      <v-toolbar-side-icon class="hidden-md-and-up" @click.native.stop="sidebar = !sidebar" />
+      <v-toolbar-items class="hidden-sm-and-down" v-for="item in items">
+        <v-toolbar-item :href="item.href" router>{{item.title}}</v-toolbar-item>
+      </v-toolbar-items>
+      <v-toolbar-items>
+        <v-toolbar-item class="hidden-sm-and-down" href="https://blog.ethereum.network/latest">Blog</v-toolbar-item>
+      </v-toolbar-items>
+      <v-spacer></v-spacer>
+      <div class="searchfield">
+        <v-text-field v-if="this.$route.path === '/projects'" label="filter projects" v-model="searchField"></v-text-field>
+      </div>
+    </v-toolbar>
+    <main>
+      <v-sidebar drawer v-model="sidebar">
+        <v-list>
+          <v-list-item v-for="item in items">
+            <v-list-tile :href="item.href" router>
+              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+            </v-list-tile>
+          </v-list-item>
+          <v-list-item>
+            <v-list-tile href="https://blog.ethereum.network/latest">
+              <v-list-tile-title>Blog</v-list-tile-title>
+            </v-list-tile>
+          </v-list-item>
+        </v-list>
+      </v-sidebar>
+      <v-content>
+        <v-container fluid class="px-2">
+          <nuxt v-bind:search-field="searchField"/>
+        </v-container>
+      </v-content>
+    </main>
+    <v-footer class="grey darken-4">
+      <div class="text-xs-right">
+        <a href="https://www.linkedin.com/company/ethereum-network" target="_blank">LinkedIn</a> | <a href="https://twitter.com/EthereumNetw" target="_blank">Twitter</a> | <a href="https://github.com/EthereumNetwork/guide-website" target="_blank">Github</a> | <a href="https://www.facebook.com/EthereumNetwork/" target="_blank">Facebook</a>
+     </div>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+  // import 'wenk'
+  export default {
+    name: 'app',
+    data () {
+      return {
+        sidebar: false,
+        searchField: '',
+        items: [
+          {title: 'Home', href: '/'},
+          {title: 'Learn', href: '/learn'},
+          {title: 'Projects', href: '/projects'},
+          {title: 'Explorer', href: '/explorer'},
+          {title: 'About', href: '/about'}
+        ]
+      }
+    },
+    mounted () {
+      fetch('/api/projects')
+      .then((response) => { return response.json() })
+      .then((data) => {
+        this.$store.commit('setProjectList', { projectList: data })
+      })
+
+      fetch('https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=BTC,USD,EUR')
+      .then((response) => { return response.json() })
+      .then((price) => { this.$store.commit('setPrice', { price: price }) })
+
+      this.$store.commit('setToken', { token: this.$cookie.get('token') })
+    }
+  }
+</script>
+
+<style>
+  h1 {
+    font-size: 1.8em;
+  }
+  h2 {
+    font-size: 1.6em;
+  }
+  .searchfield {
+    float:right;
+    padding-right: 1%;
+    color: white;
+    width: 20em;
+    height: 4.5em;
+  }
+  .app  {
+    font-family: Arial, Helvetica, sans-serif;
+  }
+  .menu {
+    color: grey;
+    font-size: 1em;
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,105 +1,44 @@
-
 <template>
-  <v-app top-toolbar footer class="app">
-    <v-toolbar class="grey darken-3">
-      <v-toolbar-side-icon class="hidden-md-and-up" @click.native.stop="sidebar = !sidebar" />
-      <v-toolbar-items class="hidden-sm-and-down" v-for="item in items">
-        <v-toolbar-item :href="item.href" router>{{item.title}}</v-toolbar-item>
-      </v-toolbar-items>
-      <v-toolbar-items>
-        <v-toolbar-item class="hidden-sm-and-down" href="https://blog.ethereum.network/latest">Blog</v-toolbar-item>
-      </v-toolbar-items>
-      <v-spacer></v-spacer>
-      <div class="searchfield">
-        <v-text-field v-if="this.$route.path === '/projects'" label="filter projects" v-model="searchField"></v-text-field>
-      </div>
-    </v-toolbar>
-    <main>
-      <v-sidebar drawer v-model="sidebar">
-        <v-list>
-          <v-list-item v-for="item in items">
-            <v-list-tile :href="item.href" router>
-              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-            </v-list-tile>
-          </v-list-item>
-          <v-list-item>
-            <v-list-tile href="https://blog.ethereum.network/latest">
-              <v-list-tile-title>Blog</v-list-tile-title>
-            </v-list-tile>
-          </v-list-item>
-        </v-list>
-      </v-sidebar>
-      <v-content>
-        <v-container fluid>
-          <div class="title">
-            <h2>Main content</h2>
-            <v-btn primary>Primary button</v-btn>
-            <v-btn secondary>Secondary button</v-btn>
-            <v-btn success>Success button</v-btn>
-          </div>
-        </v-container>
-      </v-content>
-    </main>
-    <v-footer class="grey darken-4">
-      <div class="text-xs-right">
-        <a href="https://www.linkedin.com/company/ethereum-network" target="_blank">LinkedIn</a> | <a href="https://twitter.com/EthereumNetw" target="_blank">Twitter</a> | <a href="https://github.com/EthereumNetwork/guide-website" target="_blank">Github</a> | <a href="https://www.facebook.com/EthereumNetwork/" target="_blank">Facebook</a>
-     </div>
-    </v-footer>
-  </v-app>
+  <v-container fluid>
+    <img class="homeImage "src="NA.png" />
+    <v-row>
+      <v-col xs1>
+      </v-col>
+      <v-col xs10>
+        <v-text-field v-on:keyup.enter.native="submit" v-model="searchBar" label="Ethereum Network" hint="search for projects, topics, adresses, txIDs, ..."></v-text-field>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-  // import 'wenk'
-  export default {
-    name: 'app',
-    data () {
-      return {
-        sidebar: false,
-        searchField: '',
-        items: [
-          {title: 'Home', href: '/'},
-          {title: 'Learn', href: '/learn'},
-          {title: 'Projects', href: '/projects'},
-          {title: 'Explorer', href: '/explorer'},
-          {title: 'About', href: '/about'}
-        ]
+export default {
+  name: 'home',
+  props: ['searchField'],
+  data () {
+    return {
+      msg: 'the network explorer will come later...',
+      searchBar: ''
+    }
+  },
+  methods: {
+    submit () {
+      if (this.searchBar.length === 42 && this.searchBar.slice(0, 2) === '0x') {
+        this.$router.push('/address/' + this.searchBar)
+      } else if (this.searchBar.length === 66 && this.searchBar.slice(0, 2) === '0x') {
+        this.$router.push('/tx/' + this.searchBar)
+      } else {
+        this.$router.push('/projects/?q=' + this.searchBar)
       }
-    },
-    mounted () {
-      fetch('/api/projects')
-      .then((response) => { return response.json() })
-      .then((data) => {
-        this.$store.commit('setProjectList', { projectList: data })
-      })
-
-      fetch('https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=BTC,USD,EUR')
-      .then((response) => { return response.json() })
-      .then((price) => { this.$store.commit('setPrice', { price: price }) })
-
-      this.$store.commit('setToken', { token: this.$cookie.get('token') })
     }
   }
+}
 </script>
 
 <style>
-  h1 {
-    font-size: 1.8em;
-  }
-  h2 {
-    font-size: 1.6em;
-  }
-  .searchfield {
-    float:right;
-    padding-right: 1%;
-    color: white;
-    width: 20em;
-    height: 4.5em;
-  }
-  .app  {
-    font-family: Arial, Helvetica, sans-serif;
-  }
-  .menu {
-    color: grey;
-    font-size: 1em;
-  }
+.homeImage {
+  max-height: 150px;
+  display: block;
+  margin: 20px auto;
+}
 </style>
